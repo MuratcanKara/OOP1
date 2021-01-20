45dd771f1c9c60050675c65870bcc21142ceb086
Muratcan Kara <muratfivem@gmail.com>
2021-01-20 23:50:39 +0300

    Add project files.

---
+++ ProductManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP1
+{   // class isimleri pascal case 
+    class ProductManager
+    {
+        //encapsulation
+        
+        public void Add(Product product)
+        {
+            Console.WriteLine("Sisteme eklendi! " + product.ProductName);
+        }
+    
+        public void Update(Product product)
+        {
+            Console.WriteLine("Güncellendi! " + product.Id);
+        }
+    
+        
+
+    }
+}
